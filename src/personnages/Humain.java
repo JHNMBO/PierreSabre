c77414de6f05a6acc75a52cba334210c9c97eefc
JHNMBO <johan.nzengue-mombo@univ-tlse3.fr>
2023-11-13 17:07:13 +0100

    création de la classe Humain

---
+++ src/personnages/Humain.java
@@ -0,0 +1,61 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String boissonFavorite;
+	private int argent;
+	
+	public Humain(String nom, String boissonFavorite, int argent) {
+		this.nom = nom;
+		this.boissonFavorite = boissonFavorite;
+		this.argent = argent;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public int getArgent() {
+		return argent;
+	}
+	
+	public void parler(String texte) {
+		System.out.println("(" + getNom() + ") - " + texte);
+	}
+	
+	public void direBonjour() {
+		parler("Bonjour ! Je m'appelle " + getNom() + " et j'aime boire du " + boissonFavorite);
+	}
+	
+	public void boire(String boisson) {
+		parler("Mmmm, un bon verre de " + boisson + " ! GLOUPS !");
+	}
+	
+	public void acheter(String bien, int prix) {
+		int argentEnPoche = getArgent();
+		if(argentEnPoche >= prix) {
+			parler("J'ai " + argentEnPoche + " sous en poche. Je vais pouvoir m'offrir " + bien +" à " + prix + " sous");
+			perdreArgent(prix);
+		}
+		else {
+			parler("Je n'ai plus que " + argentEnPoche + " sous en poche. Je ne peux même pas m'offrir un kimono à " + prix + " sous");
+		}
+	}
+	
+	public void gagnerArgent(int gain) {
+		this.argent += gain;
+	}
+	
+	public void perdreArgent(int perte) {
+		this.argent -= perte;
+	}
+		
+	public static void main(String[] args) {
+		Humain prof = new Humain("Prof", "kombucha", 54);
+		prof.direBonjour();
+		prof.acheter("une boisson", 12);
+		prof.boire("kombucha");
+		prof.acheter("un jeu", 2);
+		prof.acheter("un kimono", 50);
+	}
+}
